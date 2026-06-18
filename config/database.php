52fcce12ff36c67a06d2cccab78eda0ab585d933
SALWA507 <salwaamzane30@gmail.com>
2026-06-18 17:50:33 +0100

    ajoute database.php

---
+++ config/database.php
@@ -0,0 +1,15 @@
+<?php
+$host="localhost";
+$dbname="pharmafefo";
+$user="root";
+$password="";
+try{
+    $pdo=new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $user,
+        $password
+    );
+    $pdo->setAttribute(PDO ::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e){
+    die($e->getMessage());
+}
